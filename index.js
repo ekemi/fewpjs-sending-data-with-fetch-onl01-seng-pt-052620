4dfd49e019ad4f83a71709e0ccbe8fb82886b026
edmundo papi <edmundopierre@yahoo.com>
2020-11-30 17:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitDate(name, email){
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users",configObj)
+  .then(function(response)) {
+    return response.json();
+  })
+  .then(function(object){
+    let h2 = document.createElement('h2');
+    h2.innerHTML= object.id;
+    document.body.appendChild(h2);
+    console.log(object);
+  })
+  .catch(function(error){
+    let h3 = doccument.createElement('h3');
+    h3.innerHTML= error.message;
+    doccument.body.appendChild(h3);
+    console.log(error.message);
+  });
+}
